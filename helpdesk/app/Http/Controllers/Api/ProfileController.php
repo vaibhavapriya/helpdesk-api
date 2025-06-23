47d8341cd2ee099d1c17d42803b52e7b309331a1
vaibhavapriyandv@gmail.com <vaibhavapriyandv@gmail.com>
2025-06-23 15:22:26 +0530

    hash facades

--- helpdesk/app/Http/Controllers/Api/ProfileController.php
+++ helpdesk/app/Http/Controllers/Api/ProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreprofileRequest;
 use App\Http\Requests\UpdateprofileRequest;
 use App\Models\Profile;
