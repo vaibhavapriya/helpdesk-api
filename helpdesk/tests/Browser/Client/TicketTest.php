02025bead99fa31562778dd9cb1aff1072a0105b
vaibhavapriyandv@gmail.com <vaibhavapriyandv@gmail.com>
2025-06-26 16:46:07 +0530

    client ticket

--- helpdesk/tests/Browser/Client/TicketTest.php
+++ helpdesk/tests/Browser/Client/TicketTest.php
@@ -41,10 +41,26 @@ class TicketTest extends DuskTestCase
                     ->pause(2000) // wait for JS to render
                     ->clickLink('Edit') // or use ->click('.btn-warning') if button
                     ->assertPathBeginsWith('/tickets/') // confirm navigation to edit
+                    ->waitFor('#edit-ticket-form', 5)
                     ->assertSee('Edit Ticket'); // or some label on the edit page
         });
     }
 
+     public function test_client_can_create_ticket()
+    {
+        $this->browse(function (Browser $browser) {
+
+            // Visit the create ticket page and fill the form
+            $browser->visit('/tickets/create') // Adjust route as needed
+                ->pause(1000)
+                ->type('title', 'Sample Ticket')
+                ->select('priority', 'medium')
+                ->type('department', 'Support')
+                ->type('description', 'This is a test ticket created by Dusk.')
+                // ->attach('attachment', __DIR__.'/files/sample.jpg') // Make sure this file exists
+                ->press('Submit');
+        });
+    }
     // public function test_client_can_delete_ticket()
     // {
     //     $this->browse(function (Browser $browser) {
