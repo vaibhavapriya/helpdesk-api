bc9a862d49edaa25d9986b037b01bcd40e14a237
vaibhavapriyandv@gmail.com <vaibhavapriyandv@gmail.com>
2025-06-27 14:52:39 +0530

    mailtest

--- helpdesk/tests/Browser/Admin/ConfigTest.php
+++ helpdesk/tests/Browser/Admin/ConfigTest.php
@@ -33,7 +33,6 @@ class ConfigTest extends DuskTestCase
     public function test_admin_can_create_and_delete_mail_config()
     {
         $this->browse(function (Browser $browser) {
-            $admin = User::where('role', 'admin')->first();
             $testEmail = 'testdemo@example.com';
             $browser->visit('/admin/mailconfig')
                 ->pause(1000)
@@ -46,7 +45,16 @@ class ConfigTest extends DuskTestCase
                         ->press('Save');
                 })
                 ->pause(2000)
-                ->assertSeeIn('#success', 'Email configuration added')
+                ->assertSeeIn('#success', 'Email configuration added');
+        });
+    }
+
+    public function test_admin_can_delete_mail_config()
+    {
+        $this->browse(function (Browser $browser) {
+            $testEmail = 'testdemo@example.com';
+
+            $browser->visit('/admin/mailconfig')
                 ->waitFor('.table-bordered tbody tr td', 5)
                 ->assertSeeIn('table.table-bordered tbody', $testEmail)
                 ->waitForText($testEmail, 5);
@@ -60,36 +68,6 @@ class ConfigTest extends DuskTestCase
                 ->assertSeeIn('#success', 'Email deleted successfully.');
         });
     }
-
-    // public function test_admin_can_delete_mail_config()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $admin = User::where('role', 'admin')->first();
-    //         $testEmail = 'test' . rand(1000, 9999) . '@example.com';
-
-    //         $this->loginAsUser($browser, $admin);
-
-    //         $browser->visit('/admin/mailconfig')
-    //             ->pause(1000)
-    //             ->press('Add Email')
-    //             ->pause(1000)
-    //             ->whenAvailable('#addEmailModal', function (Browser $modal) use ($testEmail) {
-    //                 $modal->type('email', $testEmail)
-    //                     ->type('name', 'ToDelete Test')
-    //                     ->type('passcode', 'secret')
-    //                     ->press('Save');
-    //             })
-    //             ->pause(2000)
-    //             ->waitForText($testEmail)
-    //             ->assertSee($testEmail)
-    //             ->with('#emailTableBody', function ($table) use ($testEmail) {
-    //                 $table->assertSee($testEmail);
-    //                 $table->click('button.delete-email');
-    //             })
-    //             ->pause(1000)
-    //             ->assertSeeIn('#success', 'Email deleted successfully.');
-    //     });
-    // }
 
 
     // public function test_admin_can_view_and_set_queue_config()
