f9a46f10d75c8b7dab92ff91ec9f1a34495430be
vaibhavapriyandv@gmail.com <vaibhavapriyandv@gmail.com>
2025-06-27 11:14:29 +0530

    lang

--- helpdesk/resources/views/guest/forgotPassword.blade.php
+++ helpdesk/resources/views/guest/forgotPassword.blade.php
@@ -24,6 +24,43 @@
 <script>
   const token = 'Bearer ' + localStorage.getItem('auth_token')?? null;
   const isAuthenticated = !!localStorage.getItem('auth_token');
+  const lang = localStorage.getItem('lang') || 'en';
+  document.getElementById('langSwitcher').value = lang;
+
+  document.addEventListener('DOMContentLoaded', async function () {
+        // Load translations
+    const loadLocaleContent = async () => {
+        const res = await fetch('/api/newticket');
+        const t = await res.json();
+
+        document.querySelector('.title').textContent = t.new_ticket;
+        document.querySelector('.information').textContent = t.info_text;
+        document.querySelector('label[for="email"]').textContent = t.email;
+        document.querySelector('label[for="title"]').textContent = t.title;
+        document.querySelector('label[for="priority"]').textContent = t.priority;
+        document.querySelector('label[for="department"]').textContent = t.department;
+        document.querySelector('label[for="description"]').textContent = t.description;
+        document.querySelector('label[for="attachment"]').textContent = t.attachment;
+        document.querySelector('.terms').textContent = t.terms;
+        document.querySelector('.confirm-button').textContent = t.submit;
+
+        // Set placeholders
+        document.getElementById('email').placeholder = t.placeholders.email;
+        document.getElementById('title').placeholder = t.placeholders.title;
+        document.getElementById('department').placeholder = t.placeholders.department;
+        document.getElementById('description').placeholder = t.placeholders.description;
+
+        // Set priority options
+        document.querySelector('#priority option[value="high"]').textContent = t.high;
+        document.querySelector('#priority option[value="medium"]').textContent = t.medium;
+        document.querySelector('#priority option[value="low"]').textContent = t.low;
+    };
+    //loadHeaderTranslations();
+    await loadLocaleContent();
+    document.getElementById('langSwitcher')?.addEventListener('change', async (e) => {
+        await loadLocaleContent();
+    });
+  });
   document.getElementById('forgotPasswordForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 
